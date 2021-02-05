4c29329839355a3a69c8462d18cbfbda0d6022e1
movingpictures83 <movingpictures83@gmail.com>
2021-02-05 08:52:46 -0500

    Added outputs

--- CPU_MultiCycle.v
+++ CPU_MultiCycle.v
@@ -1,32 +1,35 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
+// Copyright (C) 2018  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
 // and other software and tools, and its AMPP partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Tue Feb  2 07:48:19 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
+// CREATED		"Fri Feb 05 08:45:55 2021"
 
 module CPU_MultiCycle(
 	clk,
 	reset,
-	Overflow
+	_PC,
+	OPCODE,
+	state
 );
 
 
 input wire	clk;
 input wire	reset;
-output wire	Overflow;
+output wire	[31:0] _PC;
+output wire	[31:26] OPCODE;
+output wire	[3:0] state;
 
 wire	[31:0] A;
 wire	[31:0] Adr;
@@ -45,6 +48,7 @@ wire	IorD;
 wire	IRWrite;
 wire	MemToReg;
 wire	MemWrite;
+wire	[3:0] myState;
 wire	[31:0] PC;
 wire	PCEn;
 wire	[31:0] PCJump;
@@ -60,6 +64,7 @@ wire	RegDst;
 wire	RegWrite;
 wire	[31:0] SrcA;
 wire	[31:0] SrcB;
+wire	wire_to_ground;
 wire	[31:0] WriteData;
 wire	[4:0] WriteRegister;
 wire	Zero;
@@ -110,7 +115,30 @@ DM_asynch	b2v_IDM(
 	.wd(SrcB),
 	.rd(ReadDat));
 
+
+Grounder	b2v_inst(
+	.Input_To_Ground(wire_to_ground));
+
 assign	PCEn = PCWriteCond_and_Zero | PCWrite;
+
+
+CTRL	b2v_inst2(
+	.clk(clk),
+	.reset(reset),
+	.Op(Instr[31:26]),
+	.PCWrite(PCWrite),
+	.PCWriteCond(PCWriteCond),
+	.IorD(IorD),
+	.MemWrite(MemWrite),
+	.IRWrite(IRWrite),
+	.MemToReg(MemToReg),
+	.ALUSrcA(ALUSrcA),
+	.RegWrite(RegWrite),
+	.RegDst(RegDst),
+	.ALUOp(ALUOp),
+	.ALUSrcB(ALUSrcB),
+	.PCSrc(PCSrc),
+	.state(myState));
 
 assign	PCWriteCond_and_Zero = PCWriteCond & Zero;
 
@@ -127,7 +155,7 @@ ALU32	b2v_MYALU(
 	.A(SrcA),
 	.alu_op(ALUControl),
 	.B(SrcB),
-	.Overflow(Overflow),
+	.Overflow(wire_to_ground),
 	.eq(Zero),
 	.Result(ALUResult));
 
@@ -136,24 +164,6 @@ ALUCtl	b2v_myALUCtl(
 	.ALUOp(ALUOp),
 	.F(Instr[5:0]),
 	.ALUControl(ALUControl));
-
-
-CTRL	b2v_myCTRL(
-	.clk(clk),
-	.reset(reset),
-	.Op(Instr[31:26]),
-	.PCWrite(PCWrite),
-	.PCWriteCond(PCWriteCond),
-	.IorD(IorD),
-	.MemWrite(MemWrite),
-	.IRWrite(IRWrite),
-	.MemToReg(MemToReg),
-	.ALUSrcA(ALUSrcA),
-	.RegWrite(RegWrite),
-	.RegDst(RegDst),
-	.ALUOp(ALUOp),
-	.ALUSrcB(ALUSrcB),
-	.PCSrc(PCSrc));
 
 
 RF	b2v_myRF(
@@ -233,5 +243,8 @@ MUX2_32	b2v_WDMUX(
 	.B(Data),
 	.Y(WriteData));
 
+assign	_PC = PC;
+assign	OPCODE[31:26] = Instr[31:26];
+assign	state = myState;
 
 endmodule
