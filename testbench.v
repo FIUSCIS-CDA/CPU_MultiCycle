2efff75733c585a80aacbf1a8ddb629271b24555
movingpictures83 <movingpictures83@gmail.com>
2022-03-27 09:58:31 -0400

    One more comment

--- testbench.v
+++ testbench.v
@@ -18,6 +18,7 @@ wire[3:0] STATE;
       myCPU.b2v_IDM.memory[1] = 'b00100000000100010000000000001011; // addi $s1, $zero, 11			4	
       myCPU.b2v_IDM.memory[2] = 'b00000000000000001001000000100000; // add $s2, $zero, zero			8
       myCPU.b2v_IDM.memory[3] = 'b00100000000011010000000000000001; // addi $t5, $zero, 1			12
+      // SHOULD ALSO WORK IF THE NEXT INSTRUCTION BECOMES: slti $t0, $s2, 11
       myCPU.b2v_IDM.memory[4] = 'b00000010010100010100000000101010; // loop1:   slt $t0, $s2, $s1            	16   
       myCPU.b2v_IDM.memory[5] = 'b00010101000011010000000000010001; // 		bne $t0, $t5, doneloop1(17)	20            
       myCPU.b2v_IDM.memory[6] = 'b00000000000000001001100000100000; // 		add $s3, $zero, $zero		24  
