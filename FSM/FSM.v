86a029657c6628d837b8e46328be6eb2eec1c8e5
Trevor Cickovski <movingpictures83@gmail.com>
2022-08-15 11:17:46 -0400

    Still were outdated

--- FSM/FSM.v
+++ FSM/FSM.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 17 16:38:13 2022"
+// CREATED		"Mon Aug 15 11:16:41 2022"
 
 module FSM(
 	addrctl,
--- FSM/NS0.v
+++ FSM/NS0.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 17 16:09:09 2022"
+// CREATED		"Mon Aug 15 11:16:31 2022"
 
 module NS0(
 	Op5,
