db45d99ef6b84f571796d9bde95708bbd403cfdd
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-16 14:22:00 -0400

    Added mult and shift

--- testbench.v
+++ testbench.v
@@ -14,10 +14,11 @@ module testbench();
    reg clk, rst;
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
-// Outputs: PC (32-bit), OPCODE (6-bit), STATE (4-bit)
+// Outputs: PC (32-bit), OPCODE (6-bit), FUNCTCODE (6-bit), STATE (5-bit)
 wire[31:0] PC;
 wire[31:26] OPCODE;
-wire[3:0] STATE;
+wire[31:26] FUNCTCODE;
+wire[4:0] STATE;
 ///////////////////////////////////////////////////////////////////////////////////
 
    integer address;
@@ -36,34 +37,34 @@ localparam CLK_PERIOD=100;
 localparam TERMINALPC=92;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   CPU_MultiCycle myCPU(.clk(clk), .reset(rst), ._PC(PC), .OPCODE(OPCODE), .state(STATE));
+   CPU_MultiCycle myCPU(.clk(clk), .reset(rst), ._PC(PC), .FUNCTCODE(FUNCTCODE), .OPCODE(OPCODE), .state(STATE));
    
    initial begin
      ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      // Initialize Instruction Memory with MIPS Bubble Sort       //          INSTRUCTION                      PC
-      myCPU.b2v_IDM.memory[0] = 'b00100000000100000000001000000000; //          addi $s0, $zero, 512  		0	
-      myCPU.b2v_IDM.memory[1] = 'b00100000000100010000000000001011; //          addi $s1, $zero, 11		4	
-      myCPU.b2v_IDM.memory[2] = 'b00000000000000001001000000100000; //          add $s2, $zero, zero		8
-      myCPU.b2v_IDM.memory[3] = 'b00100000000011010000000000000001; //          addi $t5, $zero, 1		12
-      myCPU.b2v_IDM.memory[4] = 'b00101010010010000000000000001011; // loop1:   slti $t0, $s2, 11          	16   
-      myCPU.b2v_IDM.memory[5] = 'b00010101000011010000000000010000; // 	       bne $t0, $t5, doneloop1          20            
-      myCPU.b2v_IDM.memory[6] = 'b00000000000000001001100000100000; // 	       add $s3, $zero, $zero		24  
-      myCPU.b2v_IDM.memory[7] = 'b00000010001100100111000000100010; //          sub $t6, $s1, $s2		28         
-      myCPU.b2v_IDM.memory[8] = 'b00000010011011100100100000101010; // loop2:   slt $t1, $s3, $t6		32
-      myCPU.b2v_IDM.memory[9] = 'b00010001001000000000000000001010; // 	       beq $t1, $zero, doneloop2        36        
-      myCPU.b2v_IDM.memory[10] = 'b00000000000100110101000010000000; // 	       sll $t2 = $s3 << 2       44       
-      myCPU.b2v_IDM.memory[11] = 'b00000010000010100101100000100000; // 	       add $t3, $s0, $t2        48       
-      myCPU.b2v_IDM.memory[12] = 'b10001101011101000000000000000000; // 	       lw $s4, 0($t3)           52       
-      myCPU.b2v_IDM.memory[13] = 'b10001101011101010000000000000100; // 	       lw $s5, 4($t3)           56       
-      myCPU.b2v_IDM.memory[14] = 'b00000010101101000110000000101010; // 	       slt $t4, $s5, $s4        60       
-      myCPU.b2v_IDM.memory[15] = 'b00010001100000000000000000000010; // 	       beq $t4, $zero, doneif   64        
-      myCPU.b2v_IDM.memory[16] = 'b10101101011101010000000000000000; // 	       sw $s5, 0($t3)           68       
-      myCPU.b2v_IDM.memory[17] = 'b10101101011101000000000000000100; // 	       sw $s4, 4($t3)           72       
-      myCPU.b2v_IDM.memory[18] = 'b00100010011100110000000000000001; // doneif: addi $s3, $s2, 1                 76	
-      myCPU.b2v_IDM.memory[19] = 'b00001000000000000000000000001000; // 	       j loop2  			80
-      myCPU.b2v_IDM.memory[20] = 'b00100010010100100000000000000001; //doneloop2:addi $s2, $s2, 1		84
-      myCPU.b2v_IDM.memory[21] = 'b00001000000000000000000000000100; // 	       j loop1 (3)			88
-      							            // doneloop1:                               92
+      // Initialize Instruction Memory with MIPS Bubble Sort         //          INSTRUCTION                    PC
+      myCPU.b2v_IDM.memory[0] = 'b00100000000100000000001000000000;  //          addi $s0, $zero, 512  		0	
+      myCPU.b2v_IDM.memory[1] = 'b00100000000100010000000000001011;  //          addi $s1, $zero, 11		4	
+      myCPU.b2v_IDM.memory[2] = 'b00000000000000001001000000100000;  //          add $s2, $zero, zero		8
+      myCPU.b2v_IDM.memory[3] = 'b00100000000011010000000000000001;  //          addi $t5, $zero, 1		12
+      myCPU.b2v_IDM.memory[4] = 'b00101010010010000000000000001011;  // loop1:   slti $t0, $s2, 11          	16   
+      myCPU.b2v_IDM.memory[5] = 'b00010101000011010000000000010000;  // 	 bne $t0, $t5, doneloop1        20            
+      myCPU.b2v_IDM.memory[6] = 'b00000000000000001001100000100000;  // 	 add $s3, $zero, $zero		24  
+      myCPU.b2v_IDM.memory[7] = 'b00000010001100100111000000100010;  //          sub $t6, $s1, $s2		28         
+      myCPU.b2v_IDM.memory[8] = 'b00000010011011100100100000101010;  // loop2:   slt $t1, $s3, $t6		32
+      myCPU.b2v_IDM.memory[9] = 'b00010001001000000000000000001010;  // 	 beq $t1, $zero, doneloop2      36        
+      myCPU.b2v_IDM.memory[10] = 'b00000000000100110101000010000000; // 	 sll $t2 = $s3 << 2             40       
+      myCPU.b2v_IDM.memory[11] = 'b00000010000010100101100000100000; // 	 add $t3, $s0, $t2              44       
+      myCPU.b2v_IDM.memory[12] = 'b10001101011101000000000000000000; // 	 lw $s4, 0($t3)                 48      
+      myCPU.b2v_IDM.memory[13] = 'b10001101011101010000000000000100; // 	 lw $s5, 4($t3)                 52       
+      myCPU.b2v_IDM.memory[14] = 'b00000010101101000110000000101010; // 	 slt $t4, $s5, $s4              56       
+      myCPU.b2v_IDM.memory[15] = 'b00010001100000000000000000000010; // 	 beq $t4, $zero, doneif         60        
+      myCPU.b2v_IDM.memory[16] = 'b10101101011101010000000000000000; // 	 sw $s5, 0($t3)                 64       
+      myCPU.b2v_IDM.memory[17] = 'b10101101011101000000000000000100; // 	 sw $s4, 4($t3)                 68       
+      myCPU.b2v_IDM.memory[18] = 'b00100010011100110000000000000001; // doneif:  addi $s3, $s2, 1               72	
+      myCPU.b2v_IDM.memory[19] = 'b00001000000000000000000000001000; // 	 j loop2  			76
+      myCPU.b2v_IDM.memory[20] = 'b00100010010100100000000000000001; //doneloop2:addi $s2, $s2, 1		80
+      myCPU.b2v_IDM.memory[21] = 'b00001000000000000000000000000100; // 	 j loop1 (3)			84
+      							            // doneloop1:                               88
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
       /////////////////////////////////////////////////////////////////////////////////////////////
